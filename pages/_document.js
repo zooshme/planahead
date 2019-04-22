405622b95797b601d626a512cf807419d75f8768
Ovidiu Spatacian-Tarnu <me@ovidiust.com>
2019-04-22 22:59:01 +0100

    fixes bug in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,20 @@
-import { Head } from 'next'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default () => (
-	<Head>
-		
-	</Head>
-)
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<title>Plan Ahead</title>
+					<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/611456/6413412/css/fonts.css" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+export default MyDocument
