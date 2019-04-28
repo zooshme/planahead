a88cb53d5aef22f4a4ec2fefdc8b490b7e2a4fb3
Ovidiu Spatacian-Tarnu <me@ovidiust.com>
2019-04-28 11:10:06 +0100

    adds support for typescript

--- components/WeatherInfo.tsx
+++ components/WeatherInfo.tsx
@@ -1,4 +1,51 @@
-const WeatherInfo = ({ 
+import React from 'react'
+
+interface IMain {
+    temp:string;
+    temp_min:string;
+    temp_max:string;
+    pressure:string;
+    sea_level:string;
+    grnd_level:string;
+    humidity:string;
+    temp_kf:string;
+}
+
+interface ISnow {
+    ['3h']:string;
+}
+
+interface IRain {
+    ['3h']:string;
+}
+
+interface IClouds {
+    all:string;
+}
+
+interface IWind {
+    speed:string;
+    deg:string;
+}
+
+interface IWeather {
+    id:string;
+    main:string;
+    description:string;
+    icon:string;
+}
+
+interface IProps {
+    main:IMain;
+    weather:IWeather[];
+    clouds:IClouds;
+    wind:IWind;
+    rain?:IRain;
+    snow?:ISnow;
+}
+
+
+const WeatherInfo:React.StatelessComponent<IProps> = ({ 
     main: { 
         temp, 
         temp_min, 
